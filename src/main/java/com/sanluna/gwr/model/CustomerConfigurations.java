6b03f4b31ed412c298d6e0c4501a0a7c6825da7d
Santos92 <AlexanderLundbergSantos@live.se>
2019-04-25 12:15:34 +0200

    changes for new tag

--- src/main/java/com/sanluna/gwr/model/CustomerConfigurations.java
+++ src/main/java/com/sanluna/gwr/model/CustomerConfigurations.java
@@ -1,4 +1,15 @@
 package com.sanluna.gwr.model;
 
 public class CustomerConfigurations {
+
+    private String testField;
+
+    public String getTestField() {
+        return testField;
+    }
+
+    public CustomerConfigurations setTestField(String testField) {
+        this.testField = testField;
+        return this;
+    }
 }
