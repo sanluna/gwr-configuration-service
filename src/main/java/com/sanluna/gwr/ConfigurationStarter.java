ed54fb6e0664c0c87eb4dfd9b30b9db844a6ea61
Santos92 <AlexanderLundbergSantos@live.se>
2019-05-06 16:54:59 +0200

    tenancyOff profile configs

--- src/main/java/com/sanluna/gwr/ConfigurationStarter.java
+++ src/main/java/com/sanluna/gwr/ConfigurationStarter.java
@@ -1,17 +1,16 @@
 package com.sanluna.gwr;
 
-import com.sanluna.commons.BeansAndConfigurations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Import;
 
 @EnableConfigServer
 @SpringBootApplication
-@Import(BeansAndConfigurations.class)
 public class ConfigurationStarter {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationStarter.class, args);
     }
+
+
 }
